846227795a34c2966c6e34e6029944cf9e7a1b32
Calleb Camargo <callebcamargo12@gmail.com>
2023-08-28 16:50:54 -0300

    projeto-controleFluxo-dio

---
+++ projeto-controle-fluxo-dio/src/br/com/calleb/projetoContador/Contador.java
@@ -0,0 +1,38 @@
+package br.com.calleb.projetoContador;
+
+import java.util.Scanner;
+
+/**
+ * @author Calleb
+ */
+public class Contador {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro: ");
+        int parametroUm = sc.nextInt();
+
+        System.out.println("Digite o segubdo parâmetro: ");
+        int parametroDois = sc.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm + 1;
+
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
---
+++ projeto-controle-fluxo-dio/src/br/com/calleb/projetoContador/ParametrosInvalidosException.java
@@ -0,0 +1,11 @@
+package br.com.calleb.projetoContador;
+
+/**
+ * @author Calleb
+ */
+public class ParametrosInvalidosException extends Exception {
+
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
